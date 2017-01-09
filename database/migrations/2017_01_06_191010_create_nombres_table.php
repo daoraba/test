55eaf060ad216945ee9ff81ddb6d019e875b1873
daoraba <daniel.ortega.abellan@gmail.com>
2017-01-09 18:25:24 +0100

    Sistema de migraciones con Laravel

--- database/migrations/2017_01_06_191010_create_nombres_table.php
+++ database/migrations/2017_01_06_191010_create_nombres_table.php
@@ -10,12 +10,19 @@ class CreateNombresTable extends Migration
      *
      * @return void
      */
+
+    //para crear migracion de tabla: php artisan make:migration "nombre de la migracion" --create="nombre de la tabla"
+    //para crear las tablas de las migraciones:  php artisan migrate
+    //para añadir columna de otra tabla dentro de esta tabla: php artisan make:migration "nombre de la migracion" --table=nombres
+    //nullable(): para poder poner valores nulos
+    //after():crear columna despues de la columna indicada
     public function up()
     {
         Schema::create('nombres', function (Blueprint $table) {
             $table->increments('id');
             $table->mediumText('nombre');
-            $table->timestamps();
+            $table->unsignedInteger('id_categoria')->nullable()->index();
+            $table->timestamps(); //created_at, updated_at
         });
     }
 
